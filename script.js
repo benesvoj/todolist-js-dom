bf04d3ca914e779fb132cf3b8f97819a6d8bef17
Vojtech Benes <vojtech.benes@carvago.com>
2022-06-09 23:12:48 +0200

    init

---
+++ script.js
@@ -0,0 +1,39 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var li = document.querySelectorAll("li");
+
+function inputLength() {
+  return input.value.length;
+}
+
+function createListElement() {
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
+}
+
+function addListAfterClick() {
+  if (inputLength() > 0) {
+    createListElement();
+  }
+}
+
+function addListAfterKeypress(event) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
+}
+
+li.forEach(li => {
+	li.addEventListener("click", function() {
+			li.classList.toggle("done");
+			});	
+});
+
+
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
